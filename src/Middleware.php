f43541db3a7e1ea319cf27364b009c9e90293fb9
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-10-20 20:51:54 +0200

    Add Middleware wrapper class

---
+++ src/Middleware.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conia\Http;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/** @psalm-api */
+abstract class Middleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $this->handle(
+            new Request($request),
+            function (Request $request) use ($handler): Response {
+                return new Response($handler->handle($request->unwrap()));
+            }
+        )->unwrap();
+    }
+
+    /**
+     * @param callable(Request): Response $next
+     */
+    abstract public function handle(Request $request, callable $next): Response;
+}
